804e1d780cf471bc9a35ff14686f889f3dbbe7fd
Samuel Ramos Barroso <sbarroso@ratedpower.com>
2024-03-12 12:22:37 +0000

    Rename package

--- src/main/java/com/ratedpower/challenge/App.java
+++ src/main/java/com/ratedpower/challenge/App.java
@@ -1,12 +1,12 @@
-package com.example.demo;
+package com.ratedpower.challenge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class App {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(App.class, args);
 	}
 }
--- src/test/java/com/ratedpower/challenge/AppTests.java
+++ src/test/java/com/ratedpower/challenge/AppTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.ratedpower.challenge;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class AppTests {
 
 	@Test
 	void contextLoads() {
